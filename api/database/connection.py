30d0209f271a6670dfc97f1a3c55bfaf0cf72502
udaydomadiya08 <udaydomadiya08@gmail.com>
2026-08-20 19:27:16 +0530

    Revert to default psycopg2 to support SSL

--- api/database/connection.py
+++ api/database/connection.py
@@ -3,8 +3,7 @@ from sqlalchemy.orm import sessionmaker, declarative_base
 from api.config import settings
 
 db_url = settings.database_url
-if db_url.startswith("postgres://") or db_url.startswith("postgresql://"):
-    db_url = db_url.replace("postgres://", "postgresql+pg8000://").replace("postgresql://", "postgresql+pg8000://")
+# We removed the pg8000 override to allow default psycopg2 to handle SSL properly
 
 connect_args = {"check_same_thread": False} if db_url.startswith("sqlite") else {}
 engine = create_engine(
