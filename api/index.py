439075530037d7a817cfbfd50b6cc3c4d65e63a7
udaydomadiya08 <udaydomadiya08@gmail.com>
2026-08-20 19:04:16 +0530

    Fix uvicorn reload command

--- api/index.py
+++ api/index.py
@@ -26,4 +26,4 @@ app.add_middleware(
 app.include_router(api_router, prefix="/api")
 
 if __name__ == "__main__":
-    uvicorn.run("api.main:app", host="0.0.0.0", port=8000, reload=True)
+    uvicorn.run("api.index:app", host="0.0.0.0", port=8000, reload=True)
